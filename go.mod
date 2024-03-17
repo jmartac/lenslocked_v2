272df37c9f86c5945113b50d24e8151460f04f92
jmartac <jmartac@gmail.com>
2024-03-17 17:46:53 +0000

    section 3

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/jmartac/lenslocked_v2
+
+go 1.22.1
+
+require github.com/go-chi/chi/v5 v5.0.12
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,20 +17,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1>To get in touch, please send an email to <a href=\"mailto:my@email.com\">my email</a>")
 }
 
-type Router struct{}
-
-func (Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	switch req.URL.Path {
-	case "/":
-		homeHandler(w, req)
-	case "/contact":
-		contactHandler(w, req)
-	default:
-		http.Error(w, "Not f Found ¯\\_(ツ)_/¯", http.StatusNotFound)
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>FAQ</h1>")
 }
 
 func main() {
-	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", &Router{})
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Not f Found ¯\\_(ツ)_/¯", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on http://localhost:3000...")
+	http.ListenAndServe(":3000", r)
 }
