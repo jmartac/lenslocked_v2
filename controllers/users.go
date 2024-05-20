17ac99ca19200c61564a9a86dcf653d3aaf14af9
jmartac <jmartac@gmail.com>
2024-05-20 18:35:42 +0100

    section 12

--- controllers/users.go
+++ controllers/users.go
@@ -3,13 +3,16 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/jmartac/lenslocked_v2/models"
 )
 
 type Users struct {
 	Templates struct {
 		New    Template
-		Create Template
+		SignIn Template
 	}
+	UserService *models.UserService
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +24,37 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "email is: "+r.FormValue("email"))
-	fmt.Fprint(w, "password is: "+r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", user)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
+}
+
+func (u Users) ProcessSignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email    string
+		Password string
+	}
+	data.Email = r.FormValue("email")
+	data.Password = r.FormValue("password")
+	user, err := u.UserService.Authenticate(data.Email, data.Password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User authenticated: %+v", user)
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jmartac/lenslocked_v2/controllers"
+	"github.com/jmartac/lenslocked_v2/models"
 	"github.com/jmartac/lenslocked_v2/templates"
 	"github.com/jmartac/lenslocked_v2/views"
 )
@@ -13,15 +14,29 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	usersCtrl := controllers.Users{}
+	dbConfig := models.DefaultPostgresConfig()
+	db, err := models.Open(dbConfig)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	usersCtrl := controllers.Users{
+		UserService: &models.UserService{
+			DB: db,
+		},
+	}
 	usersCtrl.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersCtrl.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/signup", usersCtrl.New)
+	r.Get("/signin", usersCtrl.SignIn)
 	r.Post("/users", usersCtrl.Create)
+	r.Post("/signin", usersCtrl.ProcessSignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not Found ¯\\_(ツ)_/¯", http.StatusNotFound)
